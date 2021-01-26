0bb1f4965271f01e457f43363cf12524a227f621
Rachel Wong <rachelwong.main@gmail.com>
2021-01-26 15:54:46 -0800

    did the rankings and reset button, table doesn't work

--- src/app.R
+++ src/app.R
@@ -2,11 +2,15 @@ library(dash)
 library(dashHtmlComponents)
 library(dashCoreComponents)
 library(dashBootstrapComponents)
+library(dplyr)
 
 app <- Dash$new(external_stylesheets = dbcThemes$BOOTSTRAP)
 
+# for table
+df = read.csv("data/processed/df_tidy.csv")
+df["Delta_happy"] = df["Happiness_score"]
 
-app$layout(htmlDiv(
+app$layout(htmlDiv( 
   list(
     # Top screen (logo, years, smiley face)
     dbcRow(
@@ -27,9 +31,41 @@ app$layout(htmlDiv(
     # Main screen layout
     dbcRow(
       list(
-        dbcCol(htmlH1("Sliders + reset")),
+        dbcCol(
+          list(
+                htmlH2("Happiness Metrics:"),
+                dbcLabel("Health"),
+                dccSlider(
+                  id="slider_health",
+                  min=0,
+                  max=10,
+                  step=1,
+                  value=5,
+                  marks=list("0" = "0", "5" = "5", "10" = "10")
+                ),
+                dbcLabel("Freedom"),
+                dccSlider(
+                  id="slider_free",
+                  min=0,
+                  max=10,
+                  step=1,
+                  value=5,
+                  marks=list("0" = "0", "5" = "5", "10" = "10")
+                ),
+                dbcLabel("Economy"),
+                dccSlider(
+                  id="slider_econ",
+                  min=0,
+                  max=10,
+                  step=1,
+                  value=5,
+                  marks=list("0" = "0", "5" = "5", "10" = "10")
+                ),
+                htmlButton("Reset", id="reset_button", n_clicks=0)
+              )
+        ),
         dbcCol(htmlH1("Map + legend")),
-        dbcCol(htmlH1("Top table"))
+        dbcCol(dccTextarea(id = "top_5_table"))
       )
     ),
     # Global metrics and individual country plots
@@ -42,4 +78,46 @@ app$layout(htmlDiv(
     )
   )
 ))
-app$run_server(debug = T)
+
+###################################################################################
+
+# App Callbacks
+
+# Slider callback
+app$callback(
+  output = list(output(id = "top_5_table", property = "value")),
+                #output(id = "top_5_table", property = "columns"),
+  params = list(input(id = "slider_health", property = "value"),
+                input(id = "slider_free", property = "value"),
+                input(id = "slider_econ", property = "value")),
+  function(health_value, free_value, econ_value) {
+    df = read.csv("data/processed/df_tidy.csv")
+    data <- filter(df, Year == 2020)
+
+    #Measure <- c("Life Expectancy", "Freedom", "GDP_per_capita") # create Measure column
+    #Value <- c(health_value, free_value, econ_value) # create Value column
+    #user_data <- data.frame(Measure, Value) # create user_data dataframe containing the inputted metrics
+
+    #country_df <- user_data[order(-Value)] # sort values in user_data (descending) and put into new dataframe
+    #col_name <- country_df[0,0] # extract the Measure with the highest importance
+    #filtered_data <- data[order(col_name)] # order the data by the Measure
+    #country_list <- filtered_data[0:10, 0] # get the list of ordered countries
+
+    return (data)
+  }
+)
+
+# Reset Button Callback, reset back to 5
+app$callback(
+  output = list(output(id = "slider_health", property = "value"),
+                output(id = "slider_free", property = "value"),
+                output(id = "slider_econ", property = "value")),
+  params = list(input(id = "reset_button", property = "n_clicks")),
+  function(heath_value, free_value, econ_value) {
+    return (list(5, 5, 5))
+  }
+)
+
+###################################################################################
+
+app$run_server(debug = F)
